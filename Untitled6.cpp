73c635d71cbd86d534a57de7cf0424076b7e19be
pakeezaanjum <152848787+pakeezaanjum@users.noreply.github.com>
2024-12-22 22:37:21 +0500

    resolved functions.cpp

--- Untitled6.cpp
+++ Untitled6.cpp
@@ -15,22 +15,22 @@ int small(int a, int b) {
 }
 
 void seed() {
-    srand(time(0));
+    srand(time(0)); 
 }
 
 int random(int max) {
-    return rand() % (max + 1);
+    return rand() % (max + 1); 
 }
 
 double distance(double x1, double y1, double x2, double y2) {
     double d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    cout << "distance between these points is " << d << endl;
+    cout << "Distance between these points is " << d << endl;
     return d;
 }
 
 double rotations(double d) {
     int rotations = d / 0.1;
-    cout << "total tyre rotations needed are " << rotations << " cm" << endl;
+    cout << "Total tyre rotations needed are " << rotations << " cm" << endl;
     return rotations;
 }
 
@@ -43,7 +43,7 @@ pair<int, int> findcd(int limit) {
 class sponge {
 public:
     int s;
-    int a;
+    int a;    
     int* array;
     int last;
 public:
@@ -56,27 +56,30 @@ public:
             array[i] = -1;
         }
     }
-
+    
     void push(int x) {
         if (a == s) {
-            cout << "full" << endl;
+            cout << "Full" << endl;
         } else {
             array[a] = x;
             a++;
         }
     }
-
-    void pop(int x) {
-        int t = 0;
-        while (array[t] != x && t < last) {
+    
+    void pop(int a) {
+        int i = 0;
+        int d = 0;    
+        int t;    
+        while (array[d] != a && d < last) {
+            d++;
+        }
+        t = d - 1;
+        while (t < last - 1) {
             t++;
+            d++;
+            array[t] = array[d];
         }
-        if (array[t] == x) {
-            for (int i = t; i < last; i++) {
-                array[i] = array[i + 1];
-            }
-            last--;
-        }
+        last--;
     }
 
     void display() {
@@ -86,7 +89,8 @@ public:
     }
 
     bool empty() {
-        return (last == -1);
+        if (last == -1) cout << "Empty sponge" << endl;
+        return last == -1;
     }
 };
 
@@ -94,16 +98,17 @@ class graph {
 public:
     int s;
     list<pair<int, int>>* l;
+    
     graph(int a) {
         s = a;
         l = new list<pair<int, int>>[a];
     }
 
     void addedge(int a, int b, int w) {
-        l[a].push_back({ b, w });
-        l[b].push_back({ a, w });
+        l[a].push_back({b, w});
+        l[b].push_back({a, w});
     }
-
+    
     void display() {
         for (int i = 0; i < s; ++i) {
             cout << "Adjacency list of vertex " << i << ": ";
@@ -117,25 +122,42 @@ public:
 
 graph generateRandomGraph(int n) {
     graph g(n);
-    // Add random edges with random weights
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
             int weight = random(10);  // Random weight between 0 and 10
             g.addedge(i, j, weight);
         }
     }
-
     return g;
 }
 
-void findShortestPath(graph& g, int source) {
+int checkstatus(int ve, sponge uv, sponge v) {
+    int check = 0;
+    int t = 0;
+    while (t != uv.last && uv.array[t] != ve) {
+        t++;
+    }
+    if (uv.array[t] == ve) {
+        check++;
+    }
+    t = 0;
+    while (t != v.last && v.array[t] != ve) {
+        t++;
+    }
+    if (v.array[t] != ve) {
+        check++;
+    }
+    return check;
+}
+
+void findShortestPath(graph &g, int source) {
     sponge unvisited(g.s);
     sponge visited(g.s);
     int r = g.s;
     int c = 3;
-    vector<vector<int>> array(r, vector<int>(c));
+    vector<vector<int>> array;
+    array.resize(r, vector<int>(c));
 
-    // Initialize the table
     for (int i = 0; i < g.s; i++) {
         array[i][0] = i;
         array[i][1] = INT_MAX;
@@ -143,25 +165,21 @@ void findShortestPath(graph& g, int source) {
     }
 
     array[source][1] = 0;
-    unvisited.push(source);
 
     while (!unvisited.empty()) {
         int u = -1;
         int min_distance = INT_MAX;
 
-        // Select the node with the smallest distance from the unvisited nodes
-        for (int i = 0; i < g.s; i++) {
-            if (array[i][1] < min_distance && !checkstatus(i, visited, unvisited)) {
-                min_distance = array[i][1];
-                u = i;
+        for (int i = 0; i < unvisited.last; i++) {
+            int node = unvisited.array[i];
+            if (array[node][1] < min_distance && !checkstatus(node, visited, unvisited)) {
+                min_distance = array[node][1];
+                u = node;
             }
         }
 
-        // Mark the node as visited and remove from unvisited list
         unvisited.pop(u);
-        visited.push(u);
 
-        // Update distances for neighbors of u
         for (auto neighbor : g.l[u]) {
             int v = neighbor.first;
             int weight = neighbor.second;
@@ -171,50 +189,25 @@ void findShortestPath(graph& g, int source) {
                 array[v][2] = u;
             }
         }
-
-        // Display the Dijkstra table after each step
-        cout << "Dijkstra table after processing node " << u << ":\n";
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                cout << array[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << "---------------------\n";
     }
 
-    // Final Dijkstra table
-    cout << "Dijkstra table (Final):\n";
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            cout << array[i][j] << " ";
+    // Print shortest paths from the source
+    cout << "Shortest paths from node " << source << ":\n";
+    for (int i = 0; i < g.s; i++) {
+        cout << "Node " << i << " distance: " << array[i][1] << ", Path: ";
+        int v = i;
+        while (array[v][2] != -1) {
+            cout << v << " <- ";
+            v = array[v][2];
         }
-        cout << endl;
+        cout << source << endl;
     }
 }
 
-int checkstatus(int ve, sponge uv, sponge v) {
-    int check = 0;
-    int t = 0;
-    while (t != uv.last && uv.array[t] != ve) {
-        t++;
-    }
-    if (uv.array[t] == ve) { check++; }
-
-    t = 0;
-    while (t != v.last && v.array[t] != ve) {
-        t++;
-    }
-    if (v.array[t] != ve) { check++; }
-
-    return check;
-}
-
-void dikefunction(graph& d, graph& g) {
+void dikefunction(graph &d, graph &g) {
     sponge unvisited(6);
     sponge visited(6);
 
-    // Push all nodes to the unvisited sponge
     for (int i = 0; i < g.s; ++i) {
         unvisited.push(i);
     }
@@ -225,26 +218,23 @@ void dikefunction(graph& d, graph& g) {
     visited.display();
     cout << endl;
 
-    // Initialize Dijkstra table (distance table)
     int r = g.s;
     int c = 3;
-    vector<vector<int>> array(r, vector<int>(c));
+    vector<vector<int>> array;  
+    array.resize(r, vector<int>(c));
 
-    // Initialize the table
     for (int i = 0; i < g.s; i++) {
         array[i][0] = i;
         array[i][1] = INT_MAX;
         array[i][2] = -1;
     }
 
-    // Set distance to source (node 0) to 0
     array[0][1] = 0;
 
     while (!unvisited.empty()) {
         int u = -1;
         int min_distance = INT_MAX;
 
-        // Select the node with the smallest distance from the unvisited nodes
         for (int i = 0; i < unvisited.last; i++) {
             int node = unvisited.array[i];
             if (array[node][1] < min_distance) {
@@ -253,10 +243,8 @@ void dikefunction(graph& d, graph& g) {
             }
         }
 
-        // Remove node from unvisited
         unvisited.pop(u);
 
-        // Update distances for neighbors of u
         for (auto neighbor : g.l[u]) {
             int v = neighbor.first;
             int weight = neighbor.second;
@@ -267,7 +255,6 @@ void dikefunction(graph& d, graph& g) {
             }
         }
 
-        // Display the Dijkstra table at each step
         cout << "Dijkstra table after processing node " << u << ":\n";
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
@@ -278,7 +265,6 @@ void dikefunction(graph& d, graph& g) {
         cout << "---------------------\n";
     }
 
-    // Final Dijkstra table
     cout << "Dijkstra table (Final):\n";
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
@@ -290,13 +276,11 @@ void dikefunction(graph& d, graph& g) {
 
 int main() {
     seed();
-
-    cout << "Here alex will find coordinates of the destination by using IR sensors installed in its machine body, to proceed, the code has generated random coordinates" << endl;
+    cout << "Here Alex will find coordinates of the destination by using IR sensors installed in its machine body, to proceed the code I have generated random coordinates" << endl;
     pair<int, int> coordinates = findcd(100);
-    cout << "coordinates are (" << coordinates.first << "," << coordinates.second << ")" << endl;
-
+    cout << "Coordinates are (" << coordinates.first << "," << coordinates.second << ")" << endl;
     int d = distance(0, 0, coordinates.first, coordinates.second);
-    cout << "now assuming radius of tyre is " << 0.1 << endl;
+    cout << "Now assuming radius of tyre is 0.1" << endl;
     rotations(d);
 
     graph g(6);
@@ -315,13 +299,8 @@ int main() {
 
     srand(time(0));
 
-    // Generate random graph with 6 nodes
     graph g1 = generateRandomGraph(6);
-
-    // Display the generated graph
     g1.display();
-
-    // Run Dijkstra's algorithm to find the shortest path
     findShortestPath(g1, 0);  // Start from node 0
 
     return 0;
